05b64c78d43ad8615e42e734d08d518111ebca1c
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-08-27 18:34:59 -0300

    resolution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ def copy_file(command: str) -> None:
         commands = command.split()
         if len(commands) == 3 and commands[1] != commands[2]:
             try:
-                with open(commands[1], "r") as file:
-                    with open(commands[2], "w") as copy_file:
-                        copy_file.write(file.read())
+                with (
+                    open(commands[1], "r") as file,
+                    open(commands[2], "w") as copy_file
+                ):
+                    copy_file.write(file.read())
             except FileNotFoundError:
                 return None
