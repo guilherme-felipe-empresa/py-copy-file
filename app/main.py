e64293f9324d74edbd943783ca20b14772b5d639
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-08-27 19:16:29 -0300

    resolution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 def copy_file(command: str) -> None:
-    if (command and "cp" in command):
-        commands = command.split()
+    if (command and command[:2] == "cp"):
+        commands = command.split(" ")
         if len(commands) == 3 and commands[1] != commands[2]:
-            try:
-                with (
-                    open(commands[1], "r") as file,
-                    open(commands[2], "w") as copy_file
-                ):
-                    copy_file.write(file.read())
-            except FileNotFoundError:
-                return None
+            command1 = commands[1][len(commands[1]) - 4:] == ".txt"
+            command2 = commands[2][len(commands[2]) - 4:] == ".txt"
+            if (command1 and command2):
+                try:
+                    with (
+                        open(commands[1], "r") as file,
+                        open(commands[2], "w") as copy_file
+                    ):
+                        copy_file.write(file.read())
+                except FileNotFoundError:
+                    return None
